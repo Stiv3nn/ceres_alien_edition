278fc9d655548fe6de2e1775fa00539ad0e24076
Stiven hoxha <stivenhoxha007@gmail.com>
2025-12-09 20:06:54 +0100

    struttura iniziale della home e importazioni necessarie

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// IMPORTIAMO LA NAVBAR
+import Navbar from '../components/Navbar'
+// IMPORTIAMO PRODUCTCARD
+import ProductCard from '../Components/ProductCard'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
