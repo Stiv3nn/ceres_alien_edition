b260fdeb407d40ef7735356d48effcaf4686728f
Stiven hoxha <stivenhoxha007@gmail.com>
2025-12-12 15:23:13 +0100

    errore risolto per vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IntroPage from "./Components/IntroPage";
+import IntroPage from "./Components/IntroPage.jsx";
 import Home from "./pages/Home";
 
 function App() {
